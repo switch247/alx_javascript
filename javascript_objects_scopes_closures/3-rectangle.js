ba6ed19f32dbbd8f79fae946718da2d43fed5fbb
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-05 20:18:00 +0300

    pr

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  //   width;
+  //   height;
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          console.log('X');
+        }
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
