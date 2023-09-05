f3b740dca5bcb27d68ab16631bb7d23f232a1d56
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-05 18:47:22 +0300

    t 0

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const list = ["C is fun","Python is cool","JavaScript is amazing"]
+// list.forEach(element => {
+//     console.log(element)
+// });
+
+for(let i = 0;i<list.length ;i++){
+    console.log(list[i])
+}
