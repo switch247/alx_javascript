d70dcfa3c2088c312b28a3d2a9bd90c7e54c4ef7
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-05 20:23:31 +0300

    t4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  //   width;
+  //   height;
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        console.log();
+      }
+    }
+  }
+
+  rotate() {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
