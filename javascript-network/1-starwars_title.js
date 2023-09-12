f9edb715e223fff02684266d31a791be4e95619a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-13 01:23:04 +0300

    tst

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const args = process.argv.slice(2)
 // slice(2) === [2:] in python
-request(`https://swapi-api.alx-tools.com/api/films/:{args[0]}`, (error, response, body)=> {
-    console.log(body.detail);
+
+request('https://swapi-api.alx-tools.com/api/films/'+args[0], (error, response, body)=> {
+    console.log(body['title']);
 });
