38ce008c58ae3deb0fd0daaa4bd0f21f66752b89
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-13 19:20:36 +0300

    t2

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -3,5 +3,9 @@ const args = process.argv.slice(2)
 // slice(2) === [2:] in python
 
 request('https://swapi-api.alx-tools.com/api/films/'+args[0], (error, response, body)=> {
-    console.log(body);
+    // Convert the string to JSON
+const jsonObject = JSON.parse(body);
+
+console.log(jsonObject.title);
+
 });
---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const args = process.argv.slice(2);
+// slice(2) === [2:] in python
+
+request(
+  'https://swapi-api.alx-tools.com/api/films/',
+  (error, response, body) => {
+    // Convert the string to JSON
+    const jsonObject = JSON.parse(body);
+    var count = 0;
+    const ls = jsonObject.results;
+    const char = 'Wedge Antilles';
+    ls.forEach((element) => {
+      // console.log(element)
+      const chars = element.characters;
+      chars.forEach((ele) => {
+        const id = ele.split('/')[5];
+        id === '18' && count++
+      });
+    });
+    // characters
+    console.log(count);
+  }
+);
