5a4a257c9cc591845b4d8dc9d33e1b240add621c
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-09-13 19:22:42 +0300

    tets

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 // slice(2) === [2:] in python
 
 request(
-  'https://swapi-api.alx-tools.com/api/films/',
+  args[0],
   (error, response, body) => {
     // Convert the string to JSON
     const jsonObject = JSON.parse(body);
